4e87914559e3222a2436df675e0940f06064c616
paula3333 <paulandrea835@gmail.com>
2022-05-27 12:11:16 -0400

    agregando ejemplos de strings

---
+++ method_numbers.rb
@@ -0,0 +1,11 @@
+puts numero_entero.next
+puts numero_siguiente=numero_entero.succ
+# numero para validar si es un numero par o impar
+puts numero_entero.odd?
+puts numero_entero.even?
+puts numero_siguiente.odd?
+puts numero_siguiente.even?
+# conversion de entero a flotante
+puts numero_siguiente/numero_entero.to_f
+
+
