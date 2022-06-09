15c8dd261833730c9ddbecaa28dc36880b0fa3f0
paula3333 <paulandrea835@gmail.com>
2022-06-09 10:14:53 -0400

    Agregando generador de listas

---
+++ basico3/puntos_intercalados.rb
@@ -0,0 +1,11 @@
+print "Ingresa cuantos numeros quieres imprimir: "
+n=gets.chomp.to_i
+
+(1..n).each do |var_block|
+    if var_block.even?
+        print var_block
+    else
+        print "."
+    end
+
+end
