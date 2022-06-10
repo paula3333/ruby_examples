991ddd2edb12a0bfde6f15b2d8657b093e917dad
paula3333 <paulandrea835@gmail.com>
2022-06-10 11:35:05 -0400

    ahora si que si

--- desafio_guiado1/emprendedor1.rb
+++ desafio_guiado1/emprendedor1.rb
@@ -1,15 +1,15 @@
 puts "CALCULO DE UTILIDADES"
 puts"--------------------------------------------------"
 
-usuarios=ARGV[0].to_i
-precio_venta=ARGV[1].to_i
+usuarios=ARGV[1].to_i
+precio_venta=ARGV[0].to_i
 gastos=ARGV[2].to_i
 puts "Usuarios totales: #{usuarios} "
 puts "Precio de venta: #{precio_venta} "
 puts "Ingrese gastos totales: #{gastos}"
 utilidad=(precio_venta*usuarios)-gastos
 if utilidad> 0
-    puts "La tienda alcanza utilidades de $#{utilidad}"
+    puts "La tienda alcanza utilidades de $#{utilidad* 0.65}"
 elsif utilidad==0
     puts "La tienda no alcanzó utilidades"
 else utilidad<0
---
+++ desafio_guiado1/emprendedor2.rb
@@ -0,0 +1,21 @@
+puts "CALCULO DE UTILIDADES"
+puts"--------------------------------------------------"
+precio_venta=ARGV[0].to_i
+usuarios=ARGV[1].to_i
+usuarios_doble=ARGV[2].to_i
+usuarios_gratis=ARGV[2].to_i
+gastos=ARGV[4].to_i
+puts "Número de usuarios comunes: #{usuarios} "
+puts "Número de usuarios premium: #{usuarios_doble} "
+puts "Número de usuarios gratuito: #{usuarios_gratis} "
+puts "Precio de venta: #{precio_venta} "
+puts "Ingrese gastos totales: #{gastos}"
+utilidad=(precio_venta*usuarios)+(precio_venta*usuarios_doble*2)+(usuarios_gratis*0)-gastos
+if utilidad> 0
+    puts "La tienda alcanza utilidades de $#{utilidad* 0.65}"
+elsif utilidad==0
+    puts "La tienda no alcanzó utilidades"
+else utilidad<0
+    puts "La tienda tiene perdidas de $#{utilidad.abs}"
+    
+end
