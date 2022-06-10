1ac4ab38ac2cad4a828ca85f35cc5bf983be7f1f
paula3333 <paulandrea835@gmail.com>
2022-06-10 11:41:10 -0400

    arreglos

--- desafio_guiado1/emprendedor1.rb
+++ desafio_guiado1/emprendedor1.rb
@@ -6,10 +6,10 @@ precio_venta=ARGV[0].to_i
 gastos=ARGV[2].to_i
 puts "Usuarios totales: #{usuarios} "
 puts "Precio de venta: #{precio_venta} "
-puts "Ingrese gastos totales: #{gastos}"
+puts "Gastos totales: #{gastos}"
 utilidad=(precio_venta*usuarios)-gastos
 if utilidad> 0
-    puts "La tienda alcanza utilidades de $#{utilidad* 0.65}"
+    puts "La tienda alcanza utilidades despues de impuestos de $#{utilidad* 0.65}"
 elsif utilidad==0
     puts "La tienda no alcanzó utilidades"
 else utilidad<0
--- desafio_guiado1/emprendedor2.rb
+++ desafio_guiado1/emprendedor2.rb
@@ -9,10 +9,10 @@ puts "Número de usuarios comunes: #{usuarios} "
 puts "Número de usuarios premium: #{usuarios_doble} "
 puts "Número de usuarios gratuito: #{usuarios_gratis} "
 puts "Precio de venta: #{precio_venta} "
-puts "Ingrese gastos totales: #{gastos}"
+puts "Gastos totales: #{gastos}"
 utilidad=(precio_venta*usuarios)+(precio_venta*usuarios_doble*2)+(usuarios_gratis*0)-gastos
 if utilidad> 0
-    puts "La tienda alcanza utilidades de $#{utilidad* 0.65}"
+    puts "La tienda alcanza utilidades despues de impuestos de $#{utilidad* 0.65}"
 elsif utilidad==0
     puts "La tienda no alcanzó utilidades"
 else utilidad<0
