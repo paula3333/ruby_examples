b7fc00c6350f26e47f98b672a6e2a28f6bb6a170
paula3333 <paulandrea835@gmail.com>
2022-06-10 10:37:45 -0400

    Cambiando a argv

--- desafio_guiado1/emprendedor1.rb
+++ desafio_guiado1/emprendedor1.rb
@@ -1,11 +1,12 @@
 puts "CALCULO DE UTILIDADES"
 puts"--------------------------------------------------"
 print "Ingrese número de usuarios: "
-usuarios=gets.chomp.to_i
+usuarios=ARGV[].to_i
+
 print "Ingrese precio de venta:  "
-precio_venta=gets.chomp.to_i
+precio_venta=ARGV[].to_i
 print "Ingrese gastos totales: "
-gastos=gets.chomp.to_i
+gastos=ARGV[].to_i
 utilidad=(precio_venta*usuarios)-gastos
 print "La tienda alcanza utilidades de #{utilidad}"
 
