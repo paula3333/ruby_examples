fafbdd711cb8c59d624a331af0c042245bf1602d
paula3333 <paulandrea835@gmail.com>
2022-06-10 23:51:41 -0400

    ultima modificacion

--- desafio_guiado1/mayor_de_3.rb
+++ desafio_guiado1/mayor_de_3.rb
@@ -3,6 +3,7 @@ puts "----------------------------------------------------"
 numero1=ARGV[0].to_i
 numero2=ARGV[1].to_i
 numero3=ARGV[2].to_i
+
 if numero1>=numero2 && numero1>=numero3
     puts "El numero mayor es #{numero1}"
 elsif numero2>=numero1 && numero2>=numero3
