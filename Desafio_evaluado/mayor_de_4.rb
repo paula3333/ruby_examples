63e9c1915132f1158960a04f1b26e15ec71e1c8c
paula3333 <paulandrea835@gmail.com>
2022-06-10 23:22:42 -0400

    se agrego el desafio evaluado

---
+++ Desafio_evaluado/mayor_de_4.rb
@@ -0,0 +1,18 @@
+puts "¿Cual es el número mayor?"
+puts "----------------------------------------------------"
+numero1=ARGV[0].to_i
+numero2=ARGV[1].to_i
+numero3=ARGV[2].to_i
+numero4=ARGV[3].to_i
+puts "Números ingresados #{numero1}, #{numero2}, #{numero3}, #{numero4} "
+if numero1>=numero2 && numero1>=numero3 && numero1>=numero4
+    puts "El numero mayor es #{numero1}"
+elsif numero2>=numero1 && numero2>=numero3 && numero2>=numero4
+    puts "El numero mayor es #{numero2}"
+elsif numero3>=numero1 && numero3>=numero2 && numero3>=numero4
+    puts "El numero mayor es #{numero3}"
+elsif numero4>=numero1 && numero4>=numero2 && numero4>=numero3
+    puts "El numero mayor es #{numero4}"
+else 
+    puts "Numeros ingresados no válidos"
+end
