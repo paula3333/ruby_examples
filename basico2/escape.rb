947ed380934615f5d49bee41facc0e1b9f54180c
paula3333 <paulandrea835@gmail.com>
2022-06-19 21:06:50 -0400

    escape

--- basico2/escape.rb
+++ basico2/escape.rb
@@ -4,3 +4,12 @@ radio = (ARGV[1].to_f)*1000
 calculo1=2 * gravedad *radio
 ve = Math.sqrt(calculo1)
 puts "La velocidad de escape es #{ve.round(2)}"
+a=18
+b=1.4
+print "#{a.class}"
+print "#{b.class}"
+puts "#{b.to_i}"
+e=b.class
+if e==Float
+    print "Lo hice"
+end
